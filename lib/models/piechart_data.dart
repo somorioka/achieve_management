64e947b532a9eb7a8121e856355604643c94dd1a
morioka <sou.kokomirai@gmail.com>
2023-04-02 18:15:24 +0900

    new: パイチャートのデータを管理するクラスを作成.まだ上手く利用できていない.

---
+++ lib/models/piechart_data.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class PieChartData{
+
+  double complitenum;
+
+  PieChartData({required this.complitenum});
+
+  final colorList = <Color>[
+    Color.fromARGB(255, 103, 202, 77),
+    Color.fromARGB(255, 88, 95, 103),
+  ];
+  
+}
